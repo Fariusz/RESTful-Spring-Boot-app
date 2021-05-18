8d4ae169ac5714f4f959581fde8d33ee24afb483
Radosław Loth <61850108+Fariusz@users.noreply.github.com>
2021-05-18 09:00:10 +0200

    Added build

---
+++ build/resources/main/data.sql
@@ -0,0 +1,4 @@
+insert into task(id,input,output,method) values (1,55,'110111','toBinary');
+insert into task(id,input,output,method) values (2,55,'10','switchDigits');
+insert into task(id,input,output,method) values (3,55,'3B','reverseDigits');
+insert into task(id,input,output,method) values (4,55,'62','sortDescending');
